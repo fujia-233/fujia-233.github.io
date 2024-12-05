6911e3cceddfcdd6d5467676d459dff4be90ab02
fujia-233 <2504397114@qq.com>
2024-12-05 10:20:08 +0800

    Site updated: 2024-12-05 10:19:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,4 +1,4 @@
-var posts=["2024/12/04/Java基础/","2024/12/03/hello-world/","2024/12/04/Nginx/"];function toRandomPost(){
+var posts=["2024/12/04/Java基础/","2024/12/04/Nginx/","2024/12/03/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };var friend_link_list=[];
     var refreshNum = 1;
